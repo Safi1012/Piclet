8bb3e7b7322c4286c0bd806f5c7c9579504d849a
Filipe Santos Correa <filipe@Filipes-MBP.fritz.box>
2015-10-13 10:59:06 +0200

    Removed unnecessary comments in 'ChallengeTableViewController'.

--- piclet/ChallengeTableViewController.swift
+++ piclet/ChallengeTableViewController.swift
@@ -29,20 +29,17 @@ class ChallengeTableViewController: UITableViewController {
         showLoadingSpinner()
 
         apiProxy.getChallenges(nil, offset: "10", success: { (challenges) -> () in
-        
             self.hideLoadingSpinner()
             self.challenges = challenges
                 
             dispatch_async(dispatch_get_main_queue(), {
                 self.tableView.reloadData()
             })
-            
         }) { (errorCode) -> () in
             self.hideLoadingSpinner()
             self.displayAlert(ErrorHandler().createErrorAlert(errorCode))
         }
     }
-    
     
     
     
@@ -89,50 +86,4 @@ class ChallengeTableViewController: UITableViewController {
         
         return cell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
