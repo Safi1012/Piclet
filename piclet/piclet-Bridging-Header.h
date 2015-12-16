4e6a9fed57418a0efff9b61bf2d8e88a8ef8efcc
Filipe Santos Correa <filipe.santoscorrea@gmail.com>
2015-12-16 09:57:46 +0100

    Added two comments to the project.

--- piclet/piclet-Bridging-Header.h
+++ piclet/piclet-Bridging-Header.h
@@ -2,10 +2,12 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+// WebImage - https://github.com/rs/SDWebImage
 #import <WebImage/UIImageView+WebCache.h>
 #import <WebImage/UIImage+WebP.h>
 #import <WebImage/SDWebImageDownloader.h>
 
+// JTSImageView - https://github.com/jaredsinclair/JTSImageViewController
 #import "JTSImageViewController.h"
 #import "JTSImageInfo.h"
 
