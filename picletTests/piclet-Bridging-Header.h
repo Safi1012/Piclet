6eaa5c678dfafef9eec155eb238dcb9a12adccd7
Filipe Santos Correa <filipe.santoscorrea@gmail.com>
2016-03-10 18:08:41 +0100

    Removed UI-Tests. Fixed UnitTests target.

---
+++ picletTests/piclet-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  piclet-Bridging-Header.h
+//  piclet
+//
+//  Created by Filipe Santos Correa on 10/03/16.
+//  Copyright © 2016 Filipe Santos Correa. All rights reserved.
+//
+
+#ifndef piclet_Bridging_Header_h
+#define piclet_Bridging_Header_h
+
+
+#endif /* piclet_Bridging_Header_h */
